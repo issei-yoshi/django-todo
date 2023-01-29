32557749b76a0be2ac208f7b8121ee90b605796a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:10:44 +0900

    Add: TaskDeleteViewクラスを作成

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 
 from todo.models import Task
@@ -15,3 +15,8 @@ class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("task-list")
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("task-list")
+    template_name = 'todo/task_delete.html'
