fbd5925f6e9bdfdadc6ebe8187c0fcbe1292b73a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 02:19:03 +0900

    Add: settings.pyにてデプロイ用の設定を実施

--- config/settings.py
+++ config/settings.py
@@ -24,12 +24,11 @@ load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'iyoshi.pythonanywhere.com']
 
 
 # Application definition
