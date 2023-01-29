abf5b28dba3fc93085e8ba8fa61f24ab9b3814c0
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 00:48:45 +0900

    Add: urlsにてTaskCreateView用のpathを設定

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todo.views import TaskListView, TaskDetailView
+from todo.views import TaskListView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("task/new/", TaskCreateView.as_view(), name="task-new"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
 ]
