5706b0b92d56c0a843af55ecf915ca534fc08df3
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:33:04 +0900

    Add: modelsにてverbose_nameを設定

--- todo/models.py
+++ todo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=30)
-    description = models.TextField(blank=True)
-    deadline = models.DateField()
+    title = models.CharField(verbose_name="タイトル", max_length=30)
+    description = models.TextField(verbose_name="詳細",blank=True)
+    deadline = models.DateField(verbose_name="締切日")
 
     def __str__(self):
         return self.title
