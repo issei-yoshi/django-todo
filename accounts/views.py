f59d69eecd9883f71b0f2655ae35be16d2e61a06
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:43:05 +0900

    Add: accounts/urlsを定義

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+class MyLoginView(LoginView):
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = True
