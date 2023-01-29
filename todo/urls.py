a599e0493000cb3eb1ae81ff5490df2ce3917c80
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:14:37 +0900

    Add: task_deleteファイルに記載してurlsを一部修正

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/new/", TaskCreateView.as_view(), name="task-new"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
-    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
