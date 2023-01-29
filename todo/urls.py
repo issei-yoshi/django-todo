3d85efae62bad463cb42e3179c1755ce88a27d1d
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:16:45 +0900

    Add: TaskUpdateViewのurlsを定義

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from todo.views import TaskListView, TaskDetailView, TaskCreateView, TaskDeleteView
+from todo.views import TaskListView, TaskDetailView, TaskCreateView, TaskDeleteView, TaskUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/new/", TaskCreateView.as_view(), name="task-new"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/<int:pk>/edit/", TaskUpdateView.as_view(), name="task-edit"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
