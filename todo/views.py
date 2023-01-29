7b584390e01425acbb089cc522979d2003c169a5
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 00:35:57 +0900

    Add: viewsにTaskDetailViewクラスを作成

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from todo.models import Task
 
 class TaskListView(ListView):
     model = Task
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "todo/task_detail.html"
