02665190d0f2f2eebb3984426190cdf1c883ab92
issei-yoshi <iyoshi.engineer@gmail.com>
2023-01-30 01:11:50 +0900

    Add: TaskDeleteViewのurlsを定義

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from todo.views import TaskListView, TaskDetailView, TaskCreateView
+from todo.views import TaskListView, TaskDetailView, TaskCreateView, TaskDeleteView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("task/new/", TaskCreateView.as_view(), name="task-new"),
     path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
 ]
